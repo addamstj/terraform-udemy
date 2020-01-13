88713170b283a8301cf70905a5b448f758c6307c
tjaddams <toby.addams@gmail.com>
2020-01-13 20:10:47 +0000

    Removed keys for remote backend

--- remote-state/backend.tf
+++ remote-state/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket     = "tj-s3-backend-course"
     key        = "terraform/tfstate.tfstate"
-    access_key = "AKIA53SMBKOTDGXUFUV5"
-    secret_key = "zTksXPW/cIPObeP/YUhkl5ct9xGfkZKgRGsL/Fqp"
+    access_key = "your_access_key"
+    secret_key = "your_secret_key"
     region     = "eu-west-2"
   }
 }
